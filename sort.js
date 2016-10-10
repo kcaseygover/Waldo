2556a8d1cabe30f2f6b12a2b1f9a18f65817eba1
Karen Casey-Gover <karen.casey@hotmail.com>
2016-10-10 13:25:18 -0300

    sort.js
    completed sort exercise

--- sort.js
+++ sort.js
@@ -1,8 +1,8 @@
-var array = [10, 2, 5, 1, 9];
+/*var array = [10, 2, 5, 1, 9];
 array.sort(function(a,b){
   return a - b
 });
-console.log(array);
+console.log(array);*/
 
 var students = [
   { id: 1, name: "bruce",    age: 40 },
@@ -11,7 +11,22 @@ var students = [
   { id: 4, name: "alex",     age: 30 }
 ];
 
+var sortStu = students.sort(function(a, b)  { //callback???
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  var ageA = a.age;
+  var ageB = b.age;
 
-//if(name === name){
-  //sort by descending age
-//}
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  if (nameA === nameB && ageA < ageB ) {
+    return 0.5;
+  }
+    return 0;
+});
+console.log(sortStu);
+
